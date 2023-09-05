5c2748ff4717086edc9ebb6e5fb05725aa26861c
Xach Hakobyan <xachhakobyan20@gmail.com>
2023-09-05 18:13:18 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const password = document.querySelector('.password')
+const username = document.querySelector('.username')
+const go = document.querySelector('.go')
+
+go.onclick = function(){
+    const passwordValue = password.value
+    const usernameValue = username.value
+
+    if(usernameValue.length < 3){
+        console.log('username min lenght 3')
+    } else if(passwordValue.length < 5){
+        console.log('password min lenght 5')
+    } else{
+        console.log(usernameValue, passwordValue)
+    }
+
+
+}
